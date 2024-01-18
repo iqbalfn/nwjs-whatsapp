684c75c1e2a62339f707dec2e8774ec09a4bb88a
Iqbal Fauzi <iqbalfawz@gmail.com>
2024-01-18 14:43:50 +0700

    First commit

---
+++ index.js
@@ -0,0 +1,86 @@
+Message = {
+    title: null,
+    onChange(title) {
+        Message.title = title
+        let re = /\(([0-9]+)\)/
+        let match = title.match(re)
+        Tray.setIcon(!!match)
+
+        if (match) {
+            Win.obj.setBadgeLabel(match[1])
+            Win.obj.requestAttention(match[1])
+        }
+    },
+    init() {
+        setInterval(e => {
+            if (Win.obj.title != Message.title) {
+                Message.onChange(Win.obj.title)
+            }
+        }, 1000)
+    }
+}
+Tray = {
+    obj: null,
+
+    addListener(tray) {
+        tray.on('click', e => {
+            Win.obj.show()
+        })
+    },
+
+    init() {
+        Tray.obj = new nw.Tray({
+            title: 'WhatsApp',
+            icon: 'icons/tray.png'
+        })
+
+        var menu = new nw.Menu()
+        menu.append(new nw.MenuItem({
+            type: 'normal',
+            label: 'Quit WhatsApp',
+            click() {
+                Win.obj.close(true)
+            }
+        }))
+        Tray.obj.menu = menu
+
+        Tray.addListener(Tray.obj)
+    },
+
+    setIcon(active) {
+        let icon = 'icons/tray'
+        if (active)
+            icon += '-notif'
+        icon += '.png'
+        Tray.obj.icon = icon
+    }
+}
+
+Win = {
+    obj: null,
+    URL: 'https://web.whatsapp.com/',
+
+    addListener(win) {
+        win.on('close', e => {
+            win.hide()
+        })
+    },
+
+    init() {
+        nw.Window.open(Win.URL, win => {
+            Win.obj = win
+            win.show()
+            Win.addListener(win)
+        })
+    }
+}
+
+Wa = {
+    init() {
+        Win.init()
+        Tray.init()
+        Message.init()
+    }
+}
+
+Wa.init()
